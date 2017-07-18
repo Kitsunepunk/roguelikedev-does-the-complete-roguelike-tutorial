abc1f07a598d642e35d2e65d1a46fdc0d388c9cd
Aurelio Tellez <auretell@gmail.com>
2017-07-18 15:43:41 -0500

    complete refactor up to part 1 of week 4
    Need to finish refactor and add in camera funtionality so I can have
    bigger maps.

--- engine.py
+++ engine.py
@@ -1,7 +1,9 @@
 import libtcodpy as libtcod
 
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
+from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
 
 
@@ -13,8 +15,12 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    map_width = 58
+    map_height = 38
     map_c_width = 58
     map_c_height = 38
+    cam_width = 58
+    cam_height = 38
     left_f_width = 60
     map_f_height = 40
 
@@ -22,29 +28,59 @@ def main():
 
     msg_f_height = 7
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    fov_algo = 0
+    fov_lightwalls = True
+    fov_radius = 10
+
     colors = {
-        'root_back': libtcod.Color(51, 51, 51),
+        'root_back': libtcod.Color(0, 127, 255),
         'root_fore': libtcod.Color(255, 255, 255),
         'player_fore': libtcod.Color(0, 0, 0),
-        'player_back': libtcod.Color(0, 127, 255)
+        'player_back': libtcod.Color(127, 255, 0),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'light_ground': libtcod.Color(200, 180, 50),
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'back': libtcod.Color(0, 0, 0)
     }
 
     player = Entity(int(map_c_width / 2), int(map_c_height / 2), '@',
                     colors.get('player_fore'), colors.get('player_back'))
     entities = [player]
 
-    libtcod.console_set_custom_font('terminal12x12_gs_ro.png',
+    libtcod.console_set_custom_font('cp437_12x12.png',
                                     libtcod.FONT_LAYOUT_ASCII_INROW |
                                     libtcod.FONT_TYPE_GREYSCALE)
 
-    libtcod.sys_force_fullscreen_resolution(800, 600)
+    libtcod.sys_force_fullscreen_resolution(1440, 1080)
     libtcod.console_init_root(screen_width, screen_height, game_title + ' ' +
                               game_ver, False)
 
     libtcod.console_set_default_background(0, colors.get('root_back'))
     libtcod.console_set_default_foreground(0, colors.get('root_fore'))
 
+    libtcod.console_rect(0, 0, 0, screen_width, screen_height, False,
+                         libtcod.BKGND_SET)
+    libtcod.console_print_frame(0, 0, 0, left_f_width, map_f_height, False,
+                                libtcod.BKGND_SET, 'Map')
+    libtcod.console_print_frame(0, 0, 40, left_f_width, look_f_height, False,
+                                libtcod.BKGND_SET, 'Look')
+    libtcod.console_print_frame(0, 0, 43, left_f_width, msg_f_height, False,
+                                libtcod.BKGND_SET, 'MESSAGE LOG')
+
     mapcon = libtcod.console_new(map_c_width, map_c_height)
+
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width,
+                      map_height, player)
+
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -53,13 +89,18 @@ def main():
 
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(mapcon, entities, screen_width, screen_height, left_f_width,
-                   map_f_height, look_f_height, msg_f_height, map_c_width,
-                   map_c_height)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius,
+                          fov_lightwalls, fov_algo)
+
+        render_all(mapcon, entities, game_map, fov_map, fov_recompute,
+                   map_c_width, map_c_height, colors)
+
+        fov_recompute = False
 
         libtcod.console_flush()
 
-        clear_all(mapcon, entities)
+        clear_all(mapcon, entities, fov_map)
 
         action = handle_keys(key)
 
@@ -69,7 +110,9 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
--- render_functions.py
+++ render_functions.py
@@ -1,38 +1,63 @@
 import libtcodpy as libtcod
 
 
-def render_all(mapcon, entities, sw, sh, lfw, mfh, lfh, msgfh, mcw, mch):
+def render_all(mapcon, entities, game_map, fov_map, fov_recompute, mcw, mch,
+               colors):
 
-    libtcod.console_rect(0, 0, 0, sw, sh, False,
-                         libtcod.BKGND_SET)
-    libtcod.console_print_frame(0, 0, 0, lfw, mfh, False,
-                                libtcod.BKGND_SET, 'Map')
-    libtcod.console_print_frame(0, 0, 40, lfw, lfh, False,
-                                libtcod.BKGND_SET, 'Look')
-    libtcod.console_print_frame(0, 0, 43, lfw, msgfh, False,
-                                libtcod.BKGND_SET, 'MESSAGE LOG')
+    if fov_recompute:
+        # Draw all the tiles in the game map
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
+
+                if visible:
+                    if wall:
+                        libtcod.console_put_char_ex(mapcon, x, y, '#', colors.
+                                                    get('light_wall'), colors.
+                                                    get('back'))
+                    else:
+                        libtcod.console_put_char_ex(mapcon, x, y, '.', colors.
+                                                    get('light_ground'), colors.
+                                                    get('back'))
+                    game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:
+                    if wall:
+                        libtcod.console_put_char_ex(mapcon, x, y, '#', colors.
+                                                    get('dark_wall'), colors.
+                                                    get('back'))
+                    else:
+                        libtcod.console_put_char_ex(mapcon, x, y, '.', colors.
+                                                    get('dark_ground'), colors.
+                                                    get('back'))
+                elif not visible:
+                    libtcod.console_put_char_ex(mapcon, x, y, 176,
+                                                libtcod.azure,
+                                                libtcod.darkest_grey)
 
     # Draw all entities in the list
     for entity in entities:
-        draw_entity(mapcon, entity)
+        draw_entity(mapcon, entity, fov_map)
 
     blit_cons(mapcon, mcw, mch, 1, 1)
 
 
-def clear_all(mapcon, entities):
+def clear_all(mapcon, entities, fov_map):
     for entity in entities:
-        clear_entity(mapcon, entity)
+        clear_entity(mapcon, entity, fov_map)
 
 
-def draw_entity(mapcon, entity):
-    libtcod.console_put_char_ex(mapcon, entity.x, entity.y, entity.char,
-                                entity.color1, entity.color2)
+def draw_entity(mapcon, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        libtcod.console_put_char_ex(mapcon, entity.x, entity.y, entity.char,
+                                    entity.color1, entity.color2)
 
 
-def clear_entity(mapcon, entity):
-    # erase the character that represents this object
-    libtcod.console_put_char_ex(mapcon, entity.x, entity.y, ' ', libtcod.black,
-                                libtcod.white)
+def clear_entity(mapcon, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        # erase the character that represents this object
+        libtcod.console_put_char_ex(mapcon, entity.x, entity.y, '.', libtcod.black,
+                                    libtcod.Color(0, 0, 0))
 
 
 def blit_cons(con, w, h, xdst, ydst):
